6d5f87977332b0563849d24909d204f49e759ccd
DevInnovator10 <devinnovator10@gmail.com>
2020-06-14 00:00:00 -0400

    chore: update Disk 1

--- lesson_plans/urls.py
+++ lesson_plans/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
    path('documents_upload', views.upload_document, name='upload_document'),
    path('search/', views.search_view, name='search_view'),
+
    path("delete_chat/<int:chat_id>/", views.delete_chat, name="delete_chat"),
 ]
